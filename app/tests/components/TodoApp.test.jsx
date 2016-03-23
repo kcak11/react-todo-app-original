548a7c7a9fb90c632951639ac5f198a574bc5b8e
Andrew Mead <andrewjosephmead@gmail.com>
2016-03-23 10:31:19 -0400

    Add test suite for list component

---
+++ app/tests/components/TodoApp.test.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jquery');
+
+var TodoApp = require('TodoApp');
+
+describe('TodoApp', () => {
+  it('should exist', () => {
+    expect(TodoApp).toExist();
+  });
+});

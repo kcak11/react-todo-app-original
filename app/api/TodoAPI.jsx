090e6feae3e88639402b18763fa3d720ea0b4bd3
Andrew Mead <andrewjosephmead@gmail.com>
2016-03-25 13:14:12 -0400

    Add localstorage support

---
+++ app/api/TodoAPI.jsx
@@ -0,0 +1,22 @@
+var $ = require('jquery');
+
+module.exports = {
+  setTodos: function (todos) {
+    if ($.isArray(todos)) {
+      localStorage.setItem('todos', JSON.stringify(todos));
+      return todos;
+    }
+  },
+  getTodos: function () {
+    var stringTodos = localStorage.getItem('todos');
+    var todos = [];
+
+    try {
+      todos = JSON.parse(stringTodos);
+    } catch (e) {
+
+    }
+
+    return $.isArray(todos) ? todos : [];
+  }
+};
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,34 +1,21 @@
 var React = require('react');
+var uuid = require('node-uuid');
+
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
-var uuid = require('node-uuid');
+var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
   getInitialState: function () {
     return {
       showCompleted: false,
       searchText: '',
-      todos: [
-        {
-          id: uuid(),
-          text: 'Walk the dog',
-          completed: false
-        }, {
-          id: uuid(),
-          text: 'Clean the yard',
-          completed: true
-        }, {
-          id: uuid(),
-          text: 'Leave mail on porch',
-          completed: true
-        }, {
-          id: uuid(),
-          text: 'Play video games',
-          completed: false
-        }
-      ]
+      todos: TodoAPI.getTodos()
     };
+  },
+  componentDidUpdate: function () {
+    TodoAPI.setTodos(this.state.todos);
   },
   handleAddTodo: function (text) {
     this.setState({

5c9bbd02160c6bd4929755da74897768e70f0e16
Andrew Mead <andrewjosephmead@gmail.com>
2016-04-20 10:45:43 -0400

    Add tests for set password component

---
+++ app/tests/components/SetPassword.test.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import expect from 'expect';
+import $ from 'jquery'
+
+import createPromiseSpy from 'app/test-utils/promise-spy';
+import * as actions from 'actions';
+import {SetPassword} from 'SetPassword';
+
+describe('SetPassword', () => {
+  const exampleEmail = 'test@example.com';
+  const exampleOldPassword = 'password123!';
+  const exampleNewPassword = 'newpassword123!';
+  const fieldData = {
+    email: {
+      value: exampleEmail
+    },
+    oldPassword: {
+      value: exampleOldPassword
+    },
+    newPassword: {
+      value: exampleNewPassword
+    }
+  };
+
+  it('should exist', () => {
+    expect(SetPassword).toExist();
+  });
+
+  it('should dispatch change password on submit', (done) => {
+    var {spy, promiseSpy, completedPromise} = createPromiseSpy(true);
+    var setPassword = TestUtils.renderIntoDocument(<SetPassword fields={fieldData} dispatch={promiseSpy}/>);
+    var $el = $(ReactDOM.findDOMNode(setPassword));
+
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(setPassword.state.isLoading).toEqual(true);
+    expect(spy).toHaveBeenCalledWith(actions.changePassword(exampleEmail, exampleOldPassword, exampleNewPassword));
+
+    completedPromise.then(() => {
+      expect(setPassword.state.isLoading).toEqual(false);
+      done();
+    });
+  });
+});

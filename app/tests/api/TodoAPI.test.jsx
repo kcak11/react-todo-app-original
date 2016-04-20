e08da0f030fd3f4dd41b51977cb77f5d5947428e
Andrew Mead <andrewjosephmead@gmail.com>
2016-04-20 10:47:24 -0400

    Remove tests for deleted methods

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -11,48 +11,6 @@ describe('TodoAPI', () => {
     expect(TodoAPI).toExist();
   });
 
-  describe('setTodos', () => {
-    it('should set valid todos array', () => {
-      var todos = [{
-        id: 23,
-        test: 'test all files',
-        completed: false
-      }];
-      TodoAPI.setTodos(todos);
-
-      var actualTodos = JSON.parse(localStorage.getItem('todos'));
-
-      expect(actualTodos).toEqual(todos);
-    });
-
-    it('should not set invalid todos array', () => {
-      var badTodos = {a: 'b'};
-      TodoAPI.setTodos(badTodos);
-
-      expect(localStorage.getItem('todos')).toBe(null);
-    });
-  });
-
-  describe('getTodos', () => {
-    it('should return empty array for bad localstorage data', () => {
-      var actualTodos = TodoAPI.getTodos();
-      expect(actualTodos).toEqual([]);
-    });
-
-    it('should return todo if valid array in localstorage', () => {
-      var todos = [{
-        id: 23,
-        test: 'test all files',
-        completed: false
-      }];
-
-      localStorage.setItem('todos', JSON.stringify(todos));
-      var actualTodos = TodoAPI.getTodos();
-
-      expect(actualTodos).toEqual(todos);
-    });
-  });
-
   describe('filterTodos', () => {
     var todos = [{
       id: 1,

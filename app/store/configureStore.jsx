f65ed4e67e5c9a3fedefc269f3fa9b1684dda0a1
Andrew Mead <andrewjosephmead@gmail.com>
2016-04-25 09:36:57 -0400

    Move middleware into own functions

--- app/store/configureStore.jsx
+++ app/store/configureStore.jsx
@@ -2,41 +2,51 @@ import * as redux from 'redux'
 import thunk from 'redux-thunk';
 import reducer from 'reducers/index';
 
-export var configure = (initialState = {}) => {
+export var loggerMiddleware = store => next => action => {
+  console.log('Dispatch action', action);
+  let result = next(action);
+  console.log('Next state', store.getState());
+  return result
+}
 
-  // TOOD - CREATE A MIDDLEWARE File
-  const logger = store => next => action => {
-    var result;
+export var authInitialState = (initialState) => {
+  var newInitialState = {...initialState};
 
-    if (typeof action === 'function') {
-      result = action(store.dispatch, store.getState)
-    } else {
-      result = next(action)
-    }
-
-    localStorage.setItem('login', JSON.stringify(
-      store.getState().user
-    ));
-
-    return result
-  }
-
+  newInitialState.user = {};
 
   try {
     let rawLogin = localStorage.getItem('login');
 
     if (rawLogin) {
-      initialState.user = JSON.parse(rawLogin);
-    } else {
-      throw new Error();
+      newInitialState.user = JSON.parse(rawLogin);
     }
-  } catch (e) {
-    initialState.user = {};
+  } catch (e) {}
+
+  return newInitialState
+};
+
+export var authMiddleware = store => next => action => {
+  var result;
+
+  if (typeof action === 'function') {
+    result = action(store.dispatch, store.getState)
+  } else {
+    result = next(action)
   }
 
+  localStorage.setItem('login', JSON.stringify(
+    store.getState().user
+  ));
+
+  return result;
+};
+
+export var configure = (initialState = {}) => {
+  initialState = authInitialState(initialState);
 
   var store = redux.createStore(reducer, initialState, redux.compose(
-    redux.applyMiddleware(logger),
+    redux.applyMiddleware(loggerMiddleware),
+    redux.applyMiddleware(authMiddleware),
     redux.applyMiddleware(thunk),
     window.devToolsExtension ? window.devToolsExtension() : f => f
   ));

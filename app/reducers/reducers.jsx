b9c1df073e0560ac38b09ea4ace019bbbdcf2a12
Andrew Mead <andrewjosephmead@gmail.com>
2016-10-04 14:52:13 -0400

    wipe todos on logout

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -42,6 +42,8 @@ export var todosReducer = (state = [], action) => {
         ...state,
         ...action.todos
       ];
+    case 'LOGOUT':
+      return [];
     default:
       return state;
   }
